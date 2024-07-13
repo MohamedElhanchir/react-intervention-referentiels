765dbbeb0dce77149d66b198db019d344723ad71
Mohamed Elhanchir <elhanchirmohamed349@gmail.com>
2024-07-13 20:35:25 +0100

    Adding FormSection Update

--- src/components/Form/EditForm.jsx
+++ src/components/Form/EditForm.jsx
@@ -1,219 +1,133 @@
 import React, { useState, useEffect } from 'react';
-import {  useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import axios from 'axios';
-import { Modal, Button, Form } from 'react-bootstrap';
-
+import { Container, Form, Col, Row, FormControl, Accordion, InputGroup } from 'react-bootstrap';
 
 function EditForm() {
   const { id } = useParams();
-  const [fields, setFields] = useState([]);
-  const [fieldTypes, setFieldTypes] = useState([]);
-  const [selectedType, setSelectedType] = useState('');
-  const [options, setOptions] = useState([{ label: "", value: "" }]);
-  const [showModal, setShowModal] = useState(false);
-  const [fieldName, setFieldName] = useState('');
-
-  const [editingField, setEditingField] = useState(null); 
-
-const handleFieldNameChange = (event) => {
-  setFieldName(event.target.value);
-};
-
-
-  useEffect(() => {
-    const fetchFieldTypes = async () => {
-      const response = await axios.get('http://localhost:8000/api/field_types');
-      setFieldTypes(response.data.data);
-    };
-    fetchFieldTypes();
-  }, []);
+  const [formSection, setFormSection] = useState({});
+  const [editSectionId, setEditSectionId] = useState(null);
+  const [editSectionName, setEditSectionName] = useState('');
 
 
   useEffect(() => {
     const fetchFields = async () => {
-      const response = await axios.get(`http://localhost:8000/api/forms/${id}/fields`);
-      setFields(response.data);
+      const response = await axios.get(`http://localhost:8000/api/forms/${id}/fields/formSection`);
+      setFormSection(response.data);
+     // console.log(response.data);
     };
     fetchFields();
   }, [id]);
-
-
-  const handleDelete = async (field) => {
-    await axios.delete(`http://localhost:8000/api/fields/${field.id}`)
-      .then(() => {
-        setFields(fields.filter(f => f.id !== field.id));
-      })
-      .catch(error => {
-        console.error('Failed to delete the field', error);
-        alert('Failed to delete the field');
-      });
-  };
-
-
-  const handleOptionChange = (index, event) => {
-    const newOptions = [...options];
-    newOptions[index][event.target.name] = event.target.value;
-    setOptions(newOptions);
-  };
-
-
-  const addOption = () => {
-    setOptions([...options, { label: "", value: "" }]);
-  };
-
-
-  const removeOption = (index) => {
-    const newOptions = [...options];
-    newOptions.splice(index, 1);
-    setOptions(newOptions);
-  };
-
-
-  const toggleModal = () => {
-    setShowModal(!showModal);
-    setFieldName('');
-    setSelectedType('');
-    setOptions([{ label: "", value: "" }])
-    setEditingField(null); 
-  };
-
-  const openEditModal = (field) => {
-    setEditingField(field);
-    setFieldName(field.name);
-    setSelectedType(field.field_type.name);
-    setOptions(Array.isArray(field.options) ? field.options : [{ label: "", value: "" }]);
-    setShowModal(true);
-  };
   
 
+  const handleDoubleClick = (sectionId, sectionName) => {
+    setEditSectionId(sectionId);
+    setEditSectionName(sectionName);
+  };
 
-const handleSaveChanges = async () => {
-  const field_type = fieldTypes.find(type => type.name === selectedType);
+  const handleNameChange = (e) => {
+    setEditSectionName(e.target.value);
+  };
 
-  if (!fieldName || !selectedType || !field_type) {
-    alert('Veuillez remplir tous les champs');
-    return;
-  }
-
-  let opts=options.filter(option => option.label && option.value).length > 0 ? options.filter(option => option.label && option.value) : null
-  if(editingField){
-    if(selectedType !== 'Select' && selectedType !== 'Radio' && selectedType !== 'Checkbox'){
-      opts=null;
+  const handleKeyPress = (e, sectionId) => {
+    if (e.key === 'Enter') {
+      console.log(editSectionName);
+      console.log(sectionId);
     }
-  }
-  
-  const dataToSend = {
-    name: fieldName,
-    field_type_id: field_type.id,
-    options: opts,
-    form_id: id,
+      // Envoyer la requête pour mettre à jour le nom de la section
+    /*  axios.put(`http://localhost:8000/api/forms/${id}/sections/${sectionId}`, {
+        name: editSectionName,
+      })
+      .then(() => {
+        // Mettre à jour l'état local après la mise à jour réussie
+        const updatedSections = { ...formSection };
+        updatedSections[sectionId].formSection.name = editSectionName;
+        setFormSection(updatedSections);
+        setEditSectionId(null); // Quitter le mode d'édition
+      })
+      .catch(error => console.error('There was an error updating the section name:', error));
+    }*/
   };
 
-  const url = editingField ? `http://localhost:8000/api/fields/${editingField.id}` : `http://localhost:8000/api/fields`;
-  const method = editingField ? 'put' : 'post';
-
-  await axios({ method, url, data: dataToSend })
-    .then((response) => {
-      setShowModal(false);
-      response.data.data.field_type = field_type;
-      if (editingField) {
-        setFields(fields.map(f => f.id === editingField.id ? response.data.data : f));
-      } else {
-       
-        setFields([...fields, response.data.data]);
-        }
-      })
-      .catch(error => {
-        console.error('Failed to save the field', error);
-        alert('Failed to save the field');
-      });
+  const renderField = (field) => {
+    switch (field.field_type.name.toLowerCase()) {
+      case 'text':
+        return <FormControl type="text" value={field.value} disabled />;
+      case 'number':
+        return <FormControl type="number" value={field.value} disabled />;
+      case 'date':
+        return <FormControl type="date" value={field.value} disabled />;
+      case 'radio':
+        return (
+          <div>
+            {field.options?.map((option, index) => (
+              <Form.Check
+                key={index}
+                type="radio"
+                label={option}
+                name={field.name}
+                value={option}
+                checked={field.value === option}
+                disabled
+              />
+            ))}
+          </div>
+        );
+      case 'checkbox':
+        return <Form.Check type="checkbox" checked={field.value} disabled />;
+      case 'datetime-local':
+        return <FormControl type="datetime-local" value={field.value} disabled />;
+      case 'email':
+        return <FormControl type="email" value={field.value} disabled />;
+      case 'password':
+        return <FormControl type="password" value={field.value} disabled />;
+      case 'file':
+        return <FormControl type="file" disabled />;
+      case 'textarea':
+        return <FormControl as="textarea" value={field.value} disabled />;
+      default:
+        return <FormControl type="text" value={field.value} disabled />;
+    }
   };
 
   return (
-    <div className="container mt-5 col-md-6">
-        <div>
-      <Button variant="success" onClick={toggleModal}>Ajouter un nouveau field</Button>
-      
-      <Modal show={showModal} onHide={toggleModal}>
-        <Modal.Header closeButton>
-          <Modal.Title>Ajouter un nouveau field</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-        <Form.Group>
-        <Form.Label>Nom</Form.Label>
-              <Form.Control type="text" required placeholder="Entrez le nom du champ" value={fieldName} onChange={handleFieldNameChange} />
-            </Form.Group>
-            <Form.Group>
-              <Form.Label>Type</Form.Label>
-              <Form.Control as="select" value={selectedType} onChange={e => setSelectedType(e.target.value)}>
-                <option value="">Sélectionnez un type</option>
-                {fieldTypes.map((type) => (
-                  <option key={type.id} value={type.name}>{type.name}</option>
-                ))}
-              </Form.Control>
-            </Form.Group>
-            
-            {selectedType === 'Checkbox' || selectedType === 'Radio' || selectedType === 'Select' ? (
-            <Form.Group>
-              <Form.Label className="m-2">Options</Form.Label>
-              {Array.isArray(options) ?  options.map((option, index) => (
-                <div key={index} className="d-flex align-items-center">
-                  <Form.Control
-                    type="text"
-                    placeholder="Label"
-                    name="label"
-                    value={option.label}
-                    onChange={(e) => handleOptionChange(index, e)}
-                    className="m-2"
+    <Container className="mt-5">
+      <Accordion defaultActiveKey="0">
+    
+        {Object.entries(formSection).map(([sectionKey, sectionValue], index) => (
+          <Accordion.Item eventKey={String(index)} key={sectionKey}>
+              <Accordion.Header
+              onDoubleClick={() => handleDoubleClick(sectionKey, sectionValue.formSection.name)}
+            >
+              {editSectionId === sectionKey && sectionValue.formSection.id ? (
+                <InputGroup>
+                  <FormControl
+                    autoFocus
+                    value={editSectionName}
+                    onChange={handleNameChange}
+                    onKeyPress={(e) => handleKeyPress(e, sectionKey)}
                   />
-                  <Form.Control
-                    type="text"
-                    placeholder="Valeur"
-                    name="value"
-                    value={option.value}
-                    onChange={(e) => handleOptionChange(index, e)}
-                    className="m-2"
-                  />
-        <Button variant="danger" onClick={() => removeOption(index)}>Supprimer</Button>
-                </div>
-              )): null}
-              <Button onClick={addOption} className="mt-2">Ajouter une option</Button>
-            </Form.Group>
-  ) : null}
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={toggleModal}>
-            Fermer
-          </Button>
-          <Button variant="primary" onClick={handleSaveChanges}>
-            Sauvegarder les changements
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </div>
-
-      <table className="table">
-        <thead className="thead-dark">
-          <tr>
-            <th>Valeur</th>
-            <th>Type</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {Array.isArray(fields) ? fields.map((field) => (
-            <tr key={field.id}>
-              <td>{field.name}</td>
-              <td>{field.field_type.name}</td>
-              <td>
-                <button className="btn btn-primary mr-2"  onClick={() => openEditModal(field)}>Modifier</button>
-                <button className="btn btn-danger m-1" onClick={() => handleDelete(field)}>Supprimer</button>
-              </td>
-            </tr>
-          )) : null}
-        </tbody>
-      </table>
-    </div>
+                </InputGroup>
+              ) : (
+                sectionValue.formSection.name
+              )}
+            </Accordion.Header>
+            <Accordion.Body>
+              {sectionValue.fields.map((field) => (
+                <Form.Group as={Row} key={field.id} className="mb-3">
+                  <Form.Label column sm="2">
+                    {field.name}
+                  </Form.Label>
+                  <Col sm="10">
+                    {renderField(field)}
+                  </Col>
+                </Form.Group>
+              ))}
+            </Accordion.Body>
+          </Accordion.Item>
+        ))}
+      </Accordion>
+    </Container>
   );
 }
 
